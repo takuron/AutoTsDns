a4842ef0e360d04b89a33222852327b4da5af4d2
takuron <zhongsunxinghe@foxmail.com>
2020-05-04 21:26:30 +0800

    修复bug

--- src/setup.bat
+++ src/setup.bat
@@ -1,7 +1,9 @@
 ::[Bat To Exe Converter]
 ::
+::fBE1pAF6MU+EWHreyHcjLQlHcDCDN36oCbtRxOnr++WXnkwIQO1ybJyW3rqdHOIa5Hnxe4U513ZW1s4UCXs=
+::fBE1pAF6MU+EWHreyHcjLQlHcDCDN36oCbtRxOnr++WXnkwIQO1ybJyW3rqdHOIa5Hnxe4U513ZW1skNGHs=
 ::YAwzoRdxOk+EWAjk
-::fBw5plQjdCyDJGyX8VAjFC9VRxGQM2X3KrQM/Ofr0+uStkJQQPFyfIHI5r2NL+4suBW2Jdt9gzcKzItdWCQKLS34OTM9qmFMiTbVZZfR41qsGhzJtx8PSTZL1DTs3ntpMIM9ypdTgXLprRyqzvcv1HnsR6ANGm3ez6l8B5xRr0S9a0ao
+::fBw5plQjdCqDJH2B4kc8Jwtoew2MOGStFYk9/fz02/mjrF4hXeEwdrPS2buAH7JDuBWpJcNghi4ayZ8zXkhnLUWXPF9m+T4X5jDVZZXL4l60Gxq240ojAmx3iG3vgi4vX4Y5yI0K0C/e
 ::YAwzuBVtJxjWCl3EqQJgSA==
 ::ZR4luwNxJguZRRnk
 ::Yhs/ulQjdF+5
@@ -13,20 +15,20 @@
 ::egkzugNsPRvcWATEpCI=
 ::dAsiuh18IRvcCxnZtBJQ
 ::cRYluBh/LU+EWAnk
-::YxY4rhs+aU+JeA==
-::cxY6rQJ7JhzQF1fEqQJQ
-::ZQ05rAF9IBncCkqN+0xwdVs0
-::ZQ05rAF9IAHYFVzEqQJQ
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQJgZkoaHUrQXA==
+::ZQ05rAF9IBncCkqN+0xwdVsEAlXMbSXoZg==
+::ZQ05rAF9IAHYFVzEqQIRPQ9bATCHMG6pBboM7czV3Io=
 ::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
 ::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
 ::cRolqwZ3JBvQF1fEqQJQ
-::dhA7uBVwLU+EWDk=
+::dhA7uBVwLU+EWG2F/1ciJxV0Sw2WNH64Zg==
 ::YQ03rBFzNR3SWATElA==
 ::dhAmsQZ3MwfNWATElA==
 ::ZQ0/vhVqMQ3MEVWAtB9wSA==
 ::Zg8zqx1/OA3MEVWAtB9wSA==
 ::dhA7pRFwIByZRRnk
-::Zh4grVQjdCyDJGyX8VAjFC9VRxGQM2X3KrQM/Ofr0+uStkJQQPFyfIHI5raHLtwA71GpYIZj02Jf+A==
+::Zh4grVQjdCqDJH2B4kc8Jwtoew2MOGStFYk9/fz02/mjrF4hR/A8RJzezqGebuUL7yU=
 ::YB416Ek+ZW8=
 ::
 ::
@@ -42,7 +44,7 @@ echo register "%~dp0" to system path
 
 cd /d %~dp0
 cd ts-dns
-svc install
+..\svc install
 
 echo ####################
 echo Set DNS Setting
@@ -52,4 +54,9 @@ netsh interface ip set dnsservers name="WLAN" source=static address=127.0.0.1
 netsh interface ip set dnsservers name="ÒÔÌ«Íø" source=static address=127.0.0.1
 netsh interface ip add dns "WLAN" 114.114.114.114 index=2  
 netsh interface ip add dns "ÒÔÌ«Íø" 114.114.114.114 index=2
+
+echo ####################
+echo Finish
+echo #################### 
+
 pause
